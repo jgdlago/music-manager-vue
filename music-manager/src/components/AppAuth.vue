8c2236367f64a0840ace7c8d8ce78651ba769a0e
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2024-03-28 11:11:27 -0300

    :sparkles: Feat: Componentes de autenticação e header

---
+++ music-manager/src/components/AppAuth.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- Auth Modal -->
+  <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+    <!-- Modal Content -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppAuth'
+}
+</script>
