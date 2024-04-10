260ffa63a458a6d8068739fceab68805b2efe5ef
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2024-04-10 14:52:32 -0300

    :sparkles: Feat: Configuração do firebase

---
+++ music-manager/src/includes/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKi-RiqgiBBSULHRWUPOvl8l7V3NluDas",
+    authDomain: "music-185e6.firebaseapp.com",
+    projectId: "music-185e6",
+    storageBucket: "music-185e6.appspot.com",
+    appId: "1:167454364843:web:51e3ad6149866b1c6353ee"
+}
+
+export default firebase.initializeApp(firebaseConfig);
